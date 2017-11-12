a2ec3c9f9d9293e16404f9f360a47f4c82ce2435
thakkarv <thakkarv@bu.edu>
2017-11-12 18:38:29 -0500

    More args and support for mutually exclusive args
    Added arguments for retrieval and making a new locker.
    Also added mutual exclustion for arguments that should not be passed
    together using the CLI's OptionsGroup API

--- src/edu/groupfour/view/CLI.java
+++ src/edu/groupfour/view/CLI.java
@@ -8,14 +8,33 @@ import org.apache.commons.cli.*;
  public class CLI {
 
      public CLI(String [] args) {
-        this.args = args;
+         this.args = args;
+         this.options = new Options();
 
-        this.options = new Options();
-        options.addOption("h", "help", false, "Show help.");
-        options.addOption("a", "addFile", true, "Path to the file to be added to the locker.");
-        options.addOption("l", "locker", true, "Path to the locker.");
-        options.addOption("d", "deleteFile", true, "Name of the file to be deleted from the locker.");
-    }
+         // these options are mutually exclusive
+         Option help = new Option("h", "help", false, "Show help.");
+         Option add = new Option("a", "add", true, "Path to the file to be added to the locker.");
+         Option retrieve = new Option("r", "retrieve", true, "Local path of the file to be retrieved.");
+         Option init = new Option("i", "init", true, "Initialize a new locker at the input path.");
+         Option delete = new Option("d", "delete", true, "Name of the file to be deleted from the locker.");
+
+         // these are dependent on the previous mutually exclusive operations
+         Option locker = new Option("l", "locker", true, "Path to an existing locker.");
+         Option target = new Option("t", "target", true, "Path to where the file retrieved should go.");
+
+         OptionGroup operations = new OptionGroup();
+         operations.addOption(help)
+                 .addOption(add)
+                 .addOption(retrieve)
+                 .addOption(init)
+                 .addOption(delete);
+
+         operations.setRequired(true);
+
+         this.options.addOptionGroup(operations);
+         this.options.addOption(locker);
+         this.options.addOption(target);
+     }
 
     public CommandLine parse() {
         CommandLineParser parser = new DefaultParser();
@@ -29,12 +48,12 @@ import org.apache.commons.cli.*;
 
             if (cmdline.hasOption("d")) {
                 System.out.println("Deleting form locker is not yet supported.");
-                log.log(Level.INFO, "User tried using unsupported delete flag.");
+                // log.log(Level.INFO, "User tried using unsupported delete flag.");
                 System.exit(0);
             }
         }
         catch (ParseException e) {
-            log.log(Level.SEVERE, "Failed to parse command line arguments.");
+            // log.log(Level.SEVERE, "Failed to parse command line arguments.");
             showHelp();
         }
 
@@ -51,5 +70,5 @@ import org.apache.commons.cli.*;
     // members
     private String [] args;
     private Options options;
-    private static final Logger log  = Logger.getLogger(CLI.class.getName());
+    // private static final Logger log  = Logger.getLogger(CLI.class.getName());
 }
