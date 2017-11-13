80a1547d6fcff47191b7eae12c267f71ec29b3f8
thakkarv <thakkarv@bu.edu>
2017-11-12 20:26:26 -0500

    Added more framework to rabin fingerprint class and locker

--- src/edu/groupfour/model/RabinFingerPrint.java
+++ src/edu/groupfour/model/RabinFingerPrint.java
@@ -1,5 +1,39 @@
 package edu.groupfour.model;
 
-public class RabinFingerPrint implements FingerPrint {
+import sun.plugin2.jvm.CircularByteBuffer;
 
+import java.util.ArrayList;
+
+public class RabinFingerPrint implements FingerPrint {
+    private CircularByteBuffer window;
+    private String polynomial;
+    private int modPrime;
+    private int base;
+    private int windowSize; // how large the hash window is in terms of bytes
+    private int threshold; // how many LSB's need to be zoro to be considered a boundry
+
+
+    public RabinFingerPrint(String file) {
+        // default parameters of Rsync
+        this.windowSize = 48;
+        this.threshold = 13;
+        this.window = new CircularByteBuffer(this.windowSize);
+        // do more stuff here
+    }
+
+    public RabinFingerPrint(String file, int windowSize, int threshold) {
+        this.windowSize = windowSize;
+        this.threshold = threshold;
+        this.window = new CircularByteBuffer(this.windowSize);
+        // do more stuff here
+    }
+
+    public String getFingerPrint(String file) {
+
+    }
+
+    // returns the array list of indexes of where the chunks should begin
+    public ArrayList<Long> getChunkBoundries(String file) {
+
+    }
 }
