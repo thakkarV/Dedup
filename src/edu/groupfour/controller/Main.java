bf028bf7190a3d2109c598e1ac700f5a0d629b26
unknown <rahulj@bu.edu>
2017-11-27 20:40:28 -0500

    Added rabin-karp not fully functional

--- src/edu/groupfour/controller/Main.java
+++ src/edu/groupfour/controller/Main.java
@@ -1,16 +1,18 @@
 package edu.groupfour.controller;
 
 import edu.groupfour.model.Locker;
+import edu.groupfour.model.RabinFingerPrint;
 import edu.groupfour.view.CLI;
 import edu.groupfour.view.GUI;
 import org.apache.commons.cli.CommandLine;
 import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class Main {
     public static void main(String[] args) throws IOException{
         //GUI gui = new GUI();
-	    CommandLine parsedArgs = new CLI(args).parse();
+	    /*CommandLine parsedArgs = new CLI(args).parse();
 
 	    if (parsedArgs.hasOption("l")) {
 	        System.out.println(parsedArgs.getOptionValue("l"));
@@ -22,8 +24,16 @@ public class Main {
             System.out.println("Adding file " + parsedArgs.getOptionValue("a") + " to locker!");
             locker.addFile(parsedArgs.getOptionValue("a"));
         }
+*/
+        RabinFingerPrint rp = new RabinFingerPrint("test");
 
-        locker.save();
+        ArrayList<Long> indexlist = new ArrayList<Long>();
+
+        indexlist = rp.getChunkBoundaries("test.txt");
+
+        System.out.print(indexlist);
+
+//        locker.save();
 
     }
 }
--- src/edu/groupfour/model/RabinFingerPrint.java
+++ src/edu/groupfour/model/RabinFingerPrint.java
@@ -1,39 +1,103 @@
 package edu.groupfour.model;
 
 import sun.plugin2.jvm.CircularByteBuffer;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.StandardOpenOption;
 
 import java.util.ArrayList;
 
 public class RabinFingerPrint implements FingerPrint {
-    private CircularByteBuffer window;
-    private String polynomial;
-    private int modPrime;
-    private int base;
-    private int windowSize; // how large the hash window is in terms of bytes
-    private int threshold; // how many LSB's need to be zoro to be considered a boundry
-
+    //private String polynomial;
+    private int modPrime = 101;
+    private int base = 256;
+    private int windowSize = 8; // how large the hash window is in terms of bytes
+    byte[] window;
+    private int threshold = 8; // how many LSB's need to be zero to be considered a boundary
 
     public RabinFingerPrint(String file) {
         // default parameters of Rsync
-        this.windowSize = 48;
-        this.threshold = 13;
-        this.window = new CircularByteBuffer(this.windowSize);
+        byte[] window = new byte[windowSize];
         // do more stuff here
     }
 
     public RabinFingerPrint(String file, int windowSize, int threshold) {
         this.windowSize = windowSize;
         this.threshold = threshold;
-        this.window = new CircularByteBuffer(this.windowSize);
+        byte[] window = new byte[windowSize];
         // do more stuff here
     }
 
+    //Produce fingerprint after cutting a boundary, skips a bunch of unnecessary processing also good to initialize.
+/*    private int InitFingerprint(String input){
+        int outprint = 0;
+        for (int i = 0; i < input.length(); i++) {
+            outprint = (outprint*modPrime + input.charAt(i))%base;
+        }
+        return outprint;
+    }
+*/
+/*
     public String getFingerPrint(String file) {
 
     }
+*/
 
     // returns the array list of indexes of where the chunks should begin
-    public ArrayList<Long> getChunkBoundries(String file) {
+    public ArrayList<Long> getChunkBoundaries(String infile) throws IOException{
 
+        //open file
+        FileInputStream readMe = new FileInputStream(infile);
+
+        long currind = 0; //current index in file
+        byte x; //current input byte
+        long nowprint = 0 ; // current fingerprint
+        ArrayList<Long> indexlist = new ArrayList<Long>(); //list to store the chunk boundaries
+
+        byte[] window = new byte[windowSize]; //the window
+        int whereami = 0; //where are we in the window
+
+
+        x = (byte) readMe.read();
+        currind++;
+        //initloop
+        for (int l = 0; l<windowSize; l++){
+            window[l] = x;
+            nowprint = (nowprint + modPrime + x)%modPrime;
+            x = (byte) readMe.read();
+            System.out.println(nowprint);
+        }
+        x = (byte) readMe.read();
+        currind++;
+        while (x != -1){
+
+            //rabin krap
+            nowprint = ((nowprint + modPrime - window[whereami]*(base%modPrime)*base%modPrime)*base+ x)%modPrime;
+
+            //update buffer
+            window[whereami] = x;
+            whereami = (whereami+1)%windowSize;
+
+            System.out.println(nowprint);
+            //if chunk boundary, update
+            if(nowprint == 57){
+                indexlist.add(currind);
+
+                //System.out.println(nowprint);
+                //flush le buffer
+                whereami = 0;
+                for (int l = 0; l<windowSize; l++){
+                    x = (byte) readMe.read();
+                    currind++;
+                    window[l] = x;
+                    nowprint = (nowprint*modPrime + window[l])%base;
+                }
+            }
+
+            x = (byte) readMe.read();
+            currind++;
+        }
+        return indexlist;
     }
 }
